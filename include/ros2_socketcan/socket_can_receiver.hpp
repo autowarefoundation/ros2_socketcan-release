eff34fa828466cc3000a20c2a22087791f1998c9
wep21 <daisuke.nishimatsu1021@gmail.com>
2023-06-04 05:24:20 +0900

    Rebase from 'debian/rolling/ros2_socketcan'

--- include/ros2_socketcan/socket_can_receiver.hpp
+++ include/ros2_socketcan/socket_can_receiver.hpp
@@ -18,10 +18,12 @@
 #ifndef ROS2_SOCKETCAN__SOCKET_CAN_RECEIVER_HPP_
 #define ROS2_SOCKETCAN__SOCKET_CAN_RECEIVER_HPP_
 
+#include <linux/can.h>
 #include <array>
 #include <chrono>
 #include <cstring>
 #include <string>
+#include <vector>
 
 #include "ros2_socketcan/visibility_control.hpp"
 #include "ros2_socketcan/socket_can_id.hpp"
@@ -36,9 +38,53 @@ class SOCKETCAN_PUBLIC SocketCanReceiver
 {
 public:
   /// Constructor
-  explicit SocketCanReceiver(const std::string & interface = "can0");
+  explicit SocketCanReceiver(const std::string & interface = "can0", const bool enable_fd = false);
   /// Destructor
   ~SocketCanReceiver() noexcept;
+
+  /// Structure containing possible CAN filter options.
+  struct CanFilterList
+  {
+    std::vector<struct can_filter> filters;
+    can_err_mask_t error_mask = 0;
+    bool join_filters = false;
+
+    /// Default constructor
+    CanFilterList() = default;
+
+
+    /// \copydoc ParseFilters(const std::string & str)
+    explicit CanFilterList(const char * str);
+
+
+    /// \copydoc ParseFilters(const std::string & str)
+    explicit CanFilterList(const std::string & str);
+
+    /// Parse CAN filters string:\n
+    /// Filters:\n
+    /// Comma separated filters can be specified for each given CAN interface.\n
+    /// <can_id>:<can_mask>\n
+    ///         (matches when <received_can_id> & mask == can_id & mask)\n
+    /// <can_id>~<can_mask>\n
+    ///         (matches when <received_can_id> & mask != can_id & mask)\n
+    /// #<error_mask>\n
+    ///         (set error frame filter, see include/linux/can/error.h)\n
+    /// [j|J]\n
+    ///         (join the given CAN filters - logical AND semantic)\n
+    ///
+    /// CAN IDs, masks and data content are given and expected in hexadecimal values.
+    /// When can_id and can_mask are both 8 digits, they are assumed to be 29 bit EFF.
+    /// \see https://manpages.ubuntu.com/manpages/jammy/man1/candump.1.html
+    /// \param[in] str Input to be parsed.
+    /// \return Populated CanFilterList structure.
+    /// \throw std::runtime_error if string couldn't be parsed.
+    static CanFilterList ParseFilters(const std::string & str);
+  };
+
+  /// Set SocketCAN filters
+  /// \param[in] filters List of filters to be applied.
+  /// \throw std::runtime_error If filters couldn't be applied
+  void SetCanFilters(const CanFilterList & filters);
 
   /// Receive CAN data
   /// \param[out] data A buffer to be written with data bytes. Must be at least 8 bytes in size
@@ -75,11 +121,47 @@ public:
     return ret;
   }
 
+  /// Receive CAN FD data
+  /// \param[out] data A buffer to be written with data bytes. Must be at least 64 bytes in size
+  /// \param[in] timeout Maximum duration to wait for data on the file descriptor. Negative
+  ///                    durations are treated the same as zero timeout
+  /// \return The CanId for the received canfd_frame, with length appropriately populated
+  /// \throw SocketCanTimeout On timeout
+  /// \throw std::runtime_error on other errors
+  CanId receive_fd(
+    void * const data,
+    const std::chrono::nanoseconds timeout = std::chrono::nanoseconds::zero()) const;
+  /// Receive typed CAN FD data. Slightly less efficient than untyped interface; has extra copy and
+  /// branches
+  /// \tparam Type of data to receive, must be 64 bytes or smaller
+  /// \param[out] data A buffer to be written with data bytes. Must be at least 64 bytes in size
+  /// \param[in] timeout Maximum duration to wait for data on the file descriptor. Negative
+  ///                    durations are treated the same as zero timeout
+  /// \return The CanId for the received canfd_frame, with length appropriately populated
+  /// \throw SocketCanTimeout On timeout
+  /// \throw std::runtime_error If received data would not fit into provided type
+  /// \throw std::runtime_error on other errors
+  template<typename T, typename = std::enable_if_t<!std::is_pointer<T>::value>>
+  CanId receive_fd(
+    T & data,
+    const std::chrono::nanoseconds timeout = std::chrono::nanoseconds::zero()) const
+  {
+    static_assert(sizeof(data) <= MAX_FD_DATA_LENGTH, "Data type too large for CAN FD");
+    std::array<uint8_t, MAX_FD_DATA_LENGTH> data_raw{};
+    const auto ret = receive_fd(&data_raw[0U], timeout);
+    if (ret.length() != sizeof(data)) {
+      throw std::runtime_error{"Received CAN FD data is of size incompatible with provided type!"};
+    }
+    (void)std::memcpy(&data, &data_raw[0U], ret.length());
+    return ret;
+  }
+
 private:
   // Wait for file descriptor to be available to send data via select()
   SOCKETCAN_LOCAL void wait(const std::chrono::nanoseconds timeout) const;
 
   int32_t m_file_descriptor;
+  bool m_enable_fd;
 };  // class SocketCanReceiver
 
 }  // namespace socketcan
