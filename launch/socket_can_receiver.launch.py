d73e8e1e57447c2df470010b45f811960871ccae
M. Fatih Cırıt <mfc@leodrive.ai>
2024-07-16 22:57:27 +0300

    Rebase from 'release/humble/ros2_socketcan'

--- launch/socket_can_receiver.launch.py
+++ launch/socket_can_receiver.launch.py
@@ -36,10 +36,13 @@ def generate_launch_description():
         namespace=TextSubstitution(text=''),
         parameters=[{
             'interface': LaunchConfiguration('interface'),
+            'enable_can_fd': LaunchConfiguration('enable_can_fd'),
             'interval_sec':
             LaunchConfiguration('interval_sec'),
+            'filters': LaunchConfiguration('filters'),
             'use_bus_time': LaunchConfiguration('use_bus_time'),
         }],
+        remappings=[('from_can_bus', LaunchConfiguration('from_can_bus_topic'))],
         output='screen')
 
     socket_can_receiver_configure_event_handler = RegisterEventHandler(
@@ -76,10 +79,35 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('interface', default_value='can0'),
+        DeclareLaunchArgument('enable_can_fd', default_value='false'),
         DeclareLaunchArgument('interval_sec', default_value='0.01'),
         DeclareLaunchArgument('use_bus_time', default_value='false'),
+        DeclareLaunchArgument('filters', default_value='0:0',
+                              description='Comma separated filters can be specified for each given'
+                                          ' CAN interface.\n'
+                                          '\t<can_id>:<can_mask>\n'
+                                          '\t\t(matches when <received_can_id> & mask == can_id & '
+                                          'mask)\n'
+                                          '\t<can_id>~<can_mask>\n'
+                                          '\t\t(matches when <received_can_id> & mask != can_id & '
+                                          'mask)\n'
+                                          '\t#<error_mask>\n'
+                                          '\t\t(set error frame filter, see include/linux/can/'
+                                          'error.h)\n'
+                                          '\t[j|J]\n'
+                                          '\t\t(join the given CAN filters - logical AND '
+                                          'semantic)\n\n'
+                                          '\tCAN IDs, masks and data content are given and '
+                                          'expected in hexadecimal values. When can_id and '
+                                          'can_mask are both 8 digits, they are assumed to '
+                                          "be 29 bit EFF. '0:0' default filter will accept "
+                                          'all data frames.\n'
+                                          '\tFor more information about syntax check: '
+                                          'https://manpages.ubuntu.com/manpages/jammy/'
+                                          'man1/candump.1.html'),
         DeclareLaunchArgument('auto_configure', default_value='true'),
         DeclareLaunchArgument('auto_activate', default_value='true'),
+        DeclareLaunchArgument('from_can_bus_topic', default_value='from_can_bus'),
         socket_can_receiver_node,
         socket_can_receiver_configure_event_handler,
         socket_can_receiver_activate_event_handler,
